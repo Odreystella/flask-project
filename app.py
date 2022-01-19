803fd2ef16ea5a5446503d5b75fe404bd61a3fe1
Oderystella <ddott59@gmail.com>
2022-01-19 21:33:11 +0900

    feat: 통계 API 구현
    - 디렉토리 구조 수정

--- app.py
+++ app.py
@@ -1,38 +1,13 @@
-import itertools
-import psycopg2
-
 from flask import Flask
-from flask_restx import Resource, Api
+from flask_restx import Api
 
-from config import DB_CONFIG
+from healthcare.controller import StatisticsView
 
 
 app = Flask(__name__)
 api = Api(app)
 
-param_dic = {
-    "port":5432,
-    "host": DB_CONFIG['HOST'],
-    "user": DB_CONFIG['USERNAME'],
-    "password": DB_CONFIG['PASSWORD'],
-    "dbname": DB_CONFIG['DB_NAME'],
-}
-
-class Database():
-    def __init__(self):
-        self.db = psycopg2.connect(
-            host=DB_CONFIG['HOST'], 
-            port=5432, 
-            user=DB_CONFIG['USERNAME'], 
-            password=DB_CONFIG['PASSWORD'], 
-            dbname=DB_CONFIG['DB_NAME'])
-        self.cursor = self.db.cursor()
-
-    def execute(self, query):
-        self.cursor.execute(query)
-        row = self.cursor.fetchall()
-
-        return list(itertools.chain(*row))
+api.add_resource(StatisticsView, "/statistics")
 
 
 if __name__=="__main__":
